2fe6ffedfd71f20c1317eeb1af283ad5eae5bca4
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-21 15:11:24 +0200

    Add error handler using next() and throw Error

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+app.use((err, req, res, next) => {
+  if (res.headerSent) {
+    return next(err);
+  }
+  res.status(err.code || 500);
+  res.json({ message: err.message || "Unknown error occurred!" })
+});
+
 app.listen(5000);
