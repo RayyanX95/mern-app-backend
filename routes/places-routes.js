db630c391dc32a6e84b56221ed7767b63532d7bc
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-21 14:36:54 +0200

    Add 'places/:pid' route functionality

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in places');
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State',
+    description: 'One of the famous sky scrapers in the world',
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516
+    },
+    address: '20 w 34th st, New York',
+    creator: 'u1',
+  }
+]
 
-  res.json({ message: 'It works' });
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+  res.json({ place });
 });
 
 module.exports = router;
