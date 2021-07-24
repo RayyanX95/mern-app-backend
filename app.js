5206b7e65f916162f11c79468ca8a059585cd026
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-24 11:10:18 +0200

    Add update place middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const HttpError = require('./models/http-error');
 
 const placesRoutes = require('./routes/places-routes');
 
@@ -9,6 +10,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+})
 
 app.use((err, req, res, next) => {
   if (res.headerSent) {
--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -46,13 +46,29 @@ const createPlace = (req, res, next) => {
     address,
     creator,
   };
-
   DUMMY_PLACES.push(createdPlace);
-
   // 201 default status when something successfully created on the server
   res.status(201).json({ place: createdPlace });
-}
+};
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({ place: updatedPlace });
+};
+
+const deletePlace = (req, res, next) => { }
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.deletePlace = deletePlace;
+exports.updatePlace = updatePlace;
