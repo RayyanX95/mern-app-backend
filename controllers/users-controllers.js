0df7a35b13bccad45f19207c0358d70ed96a09ee
Ibrahim AlRayyan <ibrahim.alrayany@gmail>
2021-08-02 07:25:54 +0200

    add mongoose to login

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -28,7 +28,7 @@ const signup = async (req, res, next) => {
   if (!errors.isEmpty()) {
     return next(new HttpError("Invalid data passed to signup. Please check your data", 422));
   };
-  
+
   const { name, email, password, places } = req.body;
   let existingUser;
   try {
@@ -58,19 +58,24 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Could not create user, email is already in use', 401);
-  }
-  const { email, password } = req.body;
-
-  const identifiedUser = DUMMY_USERS.find(u => u.email === email);
-  console.log(req.body);
-  console.log(identifiedUser);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Credentials seems to be invalid', 401);
+    console.log('errors', errors);
+    return next(new HttpError('Could not login', 401));
   };
+
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    const err = new HttpError('logging in failed', 500);
+    return next(err);
+  };
+  if (!existingUser || existingUser.password !== password) {
+    return next(new Error('Credentials seems to be invalid', 401));
+  }
 
   res.json({ message: 'Logged in' });
 };
