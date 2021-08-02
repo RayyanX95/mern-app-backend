437a3fa70e338224eed71983f43a19e8ee1c31e4
Ibrahim AlRayyan <ibrahim.alrayany@gmail>
2021-08-02 07:12:34 +0200

    add mongoose to user signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,6 +1,8 @@
 const { validationResult } = require('express-validator');
 const uuid = require('uuid');
+
 const HttpError = require('../models/http-error');
+const User = require('../models/users');
 
 const DUMMY_USERS = [
   {
@@ -21,32 +23,44 @@ const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid data passed to signup. Please check your data");
+    return next(new HttpError("Invalid data passed to signup. Please check your data", 422));
   };
-
-  const { name, email, password } = req.body;
-  const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email is already in use', 401);
+  
+  const { name, email, password, places } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    const err = new HttpError('Signing up failed', 500);
+    return next(err);
+  };
+  if (existingUser) {
+    return next(new Error('User exists already, please login instead', 422));
   }
-  const createdUser = {
-    id: uuid.v4(),
+
+  const createdUser = new User({
     name,
     email,
-    password
+    password,
+    image: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    places
+  })
+
+  try {
+    await createdUser.save();
+  } catch (error) {
+    return next(new Error('Signing up failed', 500));
   };
 
-  DUMMY_USERS.push(createdUser);
-
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
   const errors = validationResult(req);
-  if(!errors.isEmpty()) {
+  if (!errors.isEmpty()) {
     throw new HttpError('Could not create user, email is already in use', 401);
   }
   const { email, password } = req.body;
