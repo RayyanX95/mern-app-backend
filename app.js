32b194c284603304b21fe3711c1910b34ca06050
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2021-08-10 07:31:46 +0200

    fix pAtching typo to PATCH

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Request-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, pATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 
   next();
 })
